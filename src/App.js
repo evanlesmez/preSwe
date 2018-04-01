0dad3da6b48db358ad60640919b4ed22de8857bf
evan lesmez <evan.lesmez@gmail.com>
2018-04-01 16:03:39 -0400

    yeet

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Gewd Restraunts</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
@@ -18,12 +18,8 @@ class App extends Component {
   }
 }
 
+
+
+
 export default App;
 
-var restraunts = [
-  {"mando's":{
-    'rating': 5,
-    'price' : '$',
-    'hours' : '5-9'
-  }}
-]
--- src/index.js
+++ src/index.js
@@ -6,3 +6,24 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+class Restraunter extends React.Component {
+    render() {
+      return (
+        <div>
+          Hello {this.props.name}
+        </div>
+      );
+    }
+  }
+  ReactDOM.render(
+    <Restraunter name= "skwad" />
+  );
+
+  var restraunts = [
+    {"mando's":{
+      'rating': 5,
+      'price' : '$',
+      'hours' : '5-9'
+    }}
+  ]
