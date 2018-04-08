1116dd9d52c423e44f03064d185b981acf3ffc39
evanlesmez <evan.lesmez@gmail.com>
2018-04-08 19:04:58 -0400

    yeet

--- src/index.js
+++ src/index.js
@@ -9,26 +9,31 @@ const dummies = [
 ];
 const googlePlaces = 'AIzaSyAZnKYiC7hreOptN7KDJ94LIhBp43isywk'
 class Restraunter extends React.Component {
-  constructor(props){
-    super(props);
+  constructor(props){ 
+    super(props);  // You will always need these for class
     this.state = {array: dummies};
   }
   
- 
-    
-  /* componentDidMount() {
-    axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=38.036275,%20-78.500208&radius=500&type=restaurant&key='+ googlePlaces);
-}  */
-// If success then add to array, if fail then don't
+  componentDidMount() {
+    axios.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=38.036275,%20-78.500208&radius=500&type=restaurant&key='+ googlePlaces)
+      .then(function (response){
+        console.log(response);
+      })
+      .catch(function (error){
+        console.log(error);
+      });
+    // this.state.array = results;
+  }  
+
   render(){
   const listy = (
     <ul>
       {this.state.array.map((rest) => 
         <li key={rest.id}>
           <h1> Name: {rest.name} </h1>
-          <li> Rating: {rest.rating} </li>
-          <li> Price: {rest.price} </li>
-          <li> Hours: {rest.hours}</li>
+          <p> Rating: {rest.rating} </p>
+          <p> Price: {rest.price} </p>
+          <p> Hours: {rest.hours}</p>
         </li>
         
       )}
